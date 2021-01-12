b1b33ddf79121d51ec344a69834357e469540a4f
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-12 20:52:39 +0200

    feat: Create error-handling module

---
+++ error-handling/greetings/greetings.go
@@ -0,0 +1,17 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+
+	return message, nil
+}
