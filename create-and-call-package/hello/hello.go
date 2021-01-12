9f58ef4aa5eb7002dec04a16cb8a480967abcc8d
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-12 20:51:39 +0200

    feat: Create create-and-call-package module

---
+++ create-and-call-package/hello/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message := greetings.Hello("World")
+
+	fmt.Println(message)
+}
